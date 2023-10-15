13affe15e704305b2974746ce6b4cee4258dc58a
Ganesh <ganeshvelugula28@gmail.com>
2023-10-15 18:27:46 +0530

    common

--- common.sh
+++ common.sh
@@ -2,19 +2,19 @@ script_location=$(pwd)
 log=/tmp/roboshop.log
 
 print_head(){
-    echo -e "\e[1m $1 [0m"
+    echo -e "\e[1m $1 \e[0m"
 }
 
 check_status(){
     if [ $? -eq 0 ]; then
-        echo -e "\e[1;32m Success[0m"
+        echo -e "\e[1;32m Success\e[0m"
     else
-        echo -e "\e[1;31mfailure[0m"
-        echo -e "\e[31mrefer the log file for more information, log - ${log}"
+        echo -e "\e[1;31mfailure\e[0m"
+        echo -e "\e[31mrefer the log file for more information, log - ${log}\e[0m"
         exit 1
     fi
 }
 
 get_code(){
-    git clone "https://github.com/roboshop-Project/$1.git"
+    git clone "https://github.com/roboshop-Project/frontend.git"
 }
