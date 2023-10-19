e6d85542432a634ff2c759a6fabe78c2dd48eb6c
Ganesh <ganeshvelugula28@gmail.com>
2023-10-19 23:23:32 +0530

    spell /code/frontend

--- frontend.sh
+++ frontend.sh
@@ -9,11 +9,11 @@ rm -rf /usr/share/nginx/html/* &>> ${log}
 check_status
 
 print_head "Getting the files from git"
-get_code froentend &>> ${log}
+get_code frontend &>> ${log}
 check_status
 
 print_head "coping the code to defaulf location"
-cp /code/froented/* /usr/share/nginx/html/ &>> ${log}
+cp /code/frontend/* /usr/share/nginx/html/ &>> ${log}
 check_status
 
 print_head "setup nginx conf file"
