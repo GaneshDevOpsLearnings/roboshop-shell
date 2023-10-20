c423f9416ebc9ad4d7c782a7ea6490a2acb8d434
Ganesh <ganeshvelugula28@gmail.com>
2023-10-20 21:33:07 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,22 @@
+if [ -z ${root_password} ]
+then
+    echo -e "please set the root_password variable"
+    exit 1
+fi
+source common.sh
+
+print_head "disbaled the default mysql"
+dnf module disable mysql -y &>> ${log}
+check_status
+
+print_head "repo files"
+cp ${script_location}/files/mysql.repo /etc/yum.repos.d/ &>> ${log}
+check_status
+
+print_head "install mysql"
+dnf install mysql-community-server -y &>> ${log}
+check_status
+
+print_head "Set password for root user"
+mysql_secure_installation --set-root-pass ${root_password}
+check_status
